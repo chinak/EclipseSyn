ec2a202da82633c2712c55deea641a4978e04272
niko <niko.j.zhou@pwc.com>
2016-08-11 17:37:52 +0800

    8/11 (1) last commit

---
+++ SecondKill/src/main/java/org/seckill/entity/Seckill.java
@@ -0,0 +1,18 @@
+package org.seckill.entity;
+
+import java.util.Date;
+
+
+public class Seckill {
+	
+	private long seckillId;
+	
+	private String name;
+	
+	private int number;
+	
+	private Date startTime;
+	
+	
+
+}
