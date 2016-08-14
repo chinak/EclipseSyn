3c25fa3c6f8778ee84301a4b5da337234621ead4
chinak <1339439956@qq.com>
2016-08-14 23:16:25 +0800

    Dao to be tested

---
+++ SecondKill/src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,27 @@
+package org.seckill.dao;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SeckillDaoTest {
+
+	@Test
+	public void testReduceNumber() {
+		
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryAll() {
+		fail("Not yet implemented");
+	}
+
+}
